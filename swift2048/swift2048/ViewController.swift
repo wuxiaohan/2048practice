a45e4419100988602c501fff2a4b7da090f22426
wuxiaohan <wuhan84@qq.com>
2015-08-27 21:38:39 +0800

    ****************Change Descriprion***************** add a button. ***************************************************

--- swift2048/swift2048/ViewController.swift
+++ swift2048/swift2048/ViewController.swift
@@ -20,6 +20,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func  startGame(sender:UIButton){
+        let alterView = UIAlertView()
+        alterView.title = "START"
+        alterView.message = "ARE YOU READY?"
+        alterView.addButtonWithTitle("READY GO!")
+        alterView.show()
+    }
 }
 
---
+++ swift2048/swift2048/swift2048.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var i = 0
+
+var button = UIButton()
+button.setTitle("helloworld",forState:.Normal)
+button.backgroundColor = UIColor.redColor()
+button.titleLabel!.textColor = UIColor.whiteColor()
+button.frame = CGRectMake(0, 0, 200, 30)
