4aad1600f8fc967f8876649af290a0093be83026
wuxiaohan <wuhan84@qq.com>
2015-09-08 00:11:08 +0800

    add 4*4 rectangle

--- swift2048/swift2048/mainViewController.swift
+++ swift2048/swift2048/mainViewController.swift
@@ -9,5 +9,47 @@
 import UIKit
 
 class mainViewController:UIViewController{
+    //游戏方格维度
+    var dimension:Int = 4
+    //游戏过关最大值
+    var maxnumber:Int = 2048
+    //数字格子的宽度
+    var width:CGFloat = 50
+    //格子与格子的间距
+    var padding:CGFloat = 6
     
+    //保存背景图数据
+    var backgrounds:Array<UIView>
+    
+    init(){
+        self.backgrounds = Array<UIView>()
+        super.init(nibName:nil, bundle:nil)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupBackground()
+    }
+    
+    func setupBackground(){
+        var x:CGFloat = 30
+        var y:CGFloat = 150
+        
+        for i in 0...dimension-1{
+            y = 150
+            for j in 0...dimension-1{
+                var background = UIView(frame: CGRectMake(x, y, width, width))
+                background.backgroundColor = UIColor.darkGrayColor()
+                
+                self.view.addSubview(background)
+                backgrounds.append(background)
+                y += padding + width
+            }
+            x += padding + width
+        }
+    }
 }
